732812c4762b72785bdfb9a09a1b15a74935dd8d
mihir jajodia <mihirjajodia51@gmail.com>
2024-02-03 12:44:21 +0100

    kubernetes configs

--- backend/config.js
+++ backend/config.js
@@ -2,6 +2,3 @@ export const PORT = 6001;
 
 export const mongoDBURL =
   'mongodb+srv://admin:admin@webshop-mern.how9uwm.mongodb.net/products-collection?retryWrites=true&w=majority';
-
-// Please create a free database for yourself.
-// This database will be deleted after tutorials
